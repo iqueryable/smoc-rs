a69de8b79a6b8760dbaa85227138f78f9c3f6d88
Lars Enevold <iqueryable@live.com>
2023-04-28 12:54:25 +0200

    Setup cli commands and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// Initialize project folder
+    Init {
+        #[arg(short, long)]
+        repository: String,
+
+        #[arg(short, long)]
+        template: String,
+    },
+    /// Update project folder with the latest revision of the template
+    Update {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.cmd {
+        SubCommand::Init {
+            repository,
+            template,
+        } => println!(
+            "Init project from {} with template {}",
+            repository, template
+        ),
+        SubCommand::Update {} => println!("Update project"),
+    }
 }
